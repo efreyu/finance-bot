6ec04d799a63340090e1cebe865648cc68649459
Artur Barilo <faustkeller45@gmail.com>
2021-04-26 23:05:08 +0300

    master | wip parse google sheet

--- parseFinance.py
+++ parseFinance.py
@@ -8,11 +8,22 @@ import json
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-
-def getSpreadSheetId():
+def get_spread_sheet_id():
     with open('parseSettings.json') as json_file:
         data = json.load(json_file)
         if not data['spreadsheetId']:
             print("spreadsheetId is not valid!")
         return data['spreadsheetId']
 
+
+def main():
+    spreadsheet_id = get_spread_sheet_id()
+    print('%s' % spreadsheet_id)
+    # findRange = getFindRange()
+    # print('%s' % (startRow))
+    # dataStruct = getDataStruct()
+    # parseSheet(spreadsheetId, findRange, dataStruct)
+
+
+if __name__ == '__main__':
+    main()
