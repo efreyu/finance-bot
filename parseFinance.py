8510631c78f6ac07b4e8dfa33ffe5f589321e15d
Artur Barilo <faustkeller45@gmail.com>
2021-04-27 00:34:27 +0300

    master | wip parse google sheet

--- parseFinance.py
+++ parseFinance.py
@@ -8,21 +8,93 @@ import json
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-def get_spread_sheet_id():
+
+def get_setting(name):
     with open('parseSettings.json') as json_file:
         data = json.load(json_file)
-        if not data['spreadsheetId']:
-            print("spreadsheetId is not valid!")
-        return data['spreadsheetId']
+        if not data[name]:
+            print("%s is not valid!" % name)
+        return data[name]
+
+
+def parseSheet():
+    spreadsheet_id = get_setting('spreadsheetId')
+    findDiapason = get_setting('findDiapason')
+    listPage = get_setting('listPage')
+    # parse rules:
+    # - parse categories
+    # - data
+    # - category
+    data = {}
+    # data.append("2020")
+    # data
+    # sheetName = get_sheet_name_by_id(spreadsheet_id, 1)
+    # cats = parseData(spreadsheet_id, sheetName + "!A2:A")
+    for page in get_setting('pages'):
+        pageName = get_sheet_name_by_id(spreadsheet_id, page['id'])
+        pageData = parseData(spreadsheet_id, pageName + "!" + findDiapason)
+        for item in pageData:
+            if not item[0] in data:
+                data[item[0]] = {}
+            if not item[2] in data[item[0]]:
+                data[item[0]] = {item[2]: float(item[1])}
+            else:
+                data[item[0]] = {item[2]: float(data[item[0]][item[2]]) + float(item[1])}
+    test = ""
+        # data.append()
+
+
+def parseData(spreadsheetId, findRange):
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=spreadsheetId,
+                                range=findRange).execute()
+    values = result.get('values', [])
+    return values
+
+
+def get_sheet_name_by_id(spreadsheetId, id):
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    sheet_metadata = sheet.get(spreadsheetId=spreadsheetId).execute()
+    sheets = sheet_metadata.get('sheets', '')
+    return sheets[id].get("properties", {}).get("title", "Sheet1")
 
 
 def main():
-    spreadsheet_id = get_spread_sheet_id()
-    print('%s' % spreadsheet_id)
-    # findRange = getFindRange()
-    # print('%s' % (startRow))
-    # dataStruct = getDataStruct()
-    # parseSheet(spreadsheetId, findRange, dataStruct)
+    parseSheet()
 
 
 if __name__ == '__main__':
