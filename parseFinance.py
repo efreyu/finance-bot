fed9c57c0f2f1973c86040f10b8bc60a52feeee0
Artur Barilo <faustkeller45@gmail.com>
2021-04-27 22:47:36 +0300

    master | wip parse google sheet

--- parseFinance.py
+++ parseFinance.py
@@ -8,6 +8,7 @@ import json
 from datetime import datetime
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SCOPES_EDIT = ['https://www.googleapis.com/auth/spreadsheets']
 
 
 def get_setting(name):
@@ -26,15 +27,7 @@ def parseSheet():
     incomeCats = parseData(spreadsheet_id, listPage + "!G2:G")
     incomeDate = parseData(spreadsheet_id, listPage + "!F2")[0][0]
     datetime_income = datetime.strptime(incomeDate, '%Y-%m-%d')
-    # parse rules:
-    # - data - done
-    # - category - done
-    # - calc start value - done
-    # - calc income - done
-    # - calc currencies - done
-    # - print to page
-    # sheetName = get_sheet_name_by_id(spreadsheet_id, 1)
-    # currencies = parseData(spreadsheet_id, sheetName + "!E2:E")
+
     data = {}
     for page in get_setting('pages'):
         pageName = get_sheet_name_by_id(spreadsheet_id, page['id'])
@@ -60,31 +53,22 @@ def parseSheet():
             else:
                 data[pageCur][datetime_key][item[2]] = float(data[pageCur][datetime_key][item[2]]) + float(item[1])
     result = {}
-    total = {}
     for cur in data:
         if not cur in result:
             result[cur] = {}
-            total[cur] = {}
         keylist = data[cur].keys()
         for key in sorted(keylist):
             row = data[cur][key]
-            month = datetime(key.year, key.month, 1)
+            month = str(key.year) + "-" + str(key.month)
             if not month in result[cur]:
                 result[cur][month] = {}
-                total[cur][month] = float()
             cats = row.keys()
             for cat in sorted(cats):
                 if not cat in result[cur][month]:
                     result[cur][month][cat] = row[cat]
                 else:
                     result[cur][month][cat] += row[cat]
-                if cat in incomeCats:
-                    total[cur][month] += row[cat]
-                else:
-                    total[cur][month] -= row[cat]
-
-
-    test = ""
+    return result
 
 
 def parseData(spreadsheetId, findRange):
@@ -136,8 +120,82 @@ def get_sheet_name_by_id(spreadsheetId, id):
     return sheets[id].get("properties", {}).get("title", "Sheet1")
 
 
+def create_write_service():
+    cred = None
+
+    if os.path.exists('token_write.pickle'):
+        with open('token_write.pickle', 'rb') as token:
+            cred = pickle.load(token)
+
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES_EDIT)
+            cred = flow.run_local_server()
+
+        with open('token_write.pickle', 'wb') as token:
+            pickle.dump(cred, token)
+
+    try:
+        service = build('sheets', 'v4', credentials=cred)
+        print('sheets', 'service created successfully')
+        return service
+    except Exception as e:
+        print(e)
+        return None
+
+
+def convert_to_list(data):
+    result = list()
+    spreadsheet_id = get_setting('spreadsheetId')
+    listPage = get_sheet_name_by_id(spreadsheet_id, get_setting('listPage'))
+    allCats = parseData(spreadsheet_id, listPage + "!A2:A")
+    dataWithCats = {}
+    firstRow = list()
+    for cur in data:
+        firstRow.append(str(cur))
+        if len(firstRow) == 1:
+            for item in allCats:
+                dataWithCats[item[0]] = [str(item[0])]
+        else:
+            for item in allCats:
+                dataWithCats[item[0]].append(str(item[0]))
+        for month in sorted(data[cur].keys(), reverse=True):
+            firstRow.append(str(month))
+        for month in sorted(data[cur].keys(), reverse=True):
+            count = 0
+            for cat in data[cur][month].keys():
+                if cat in dataWithCats.keys():
+                    dataWithCats[cat].append(data[cur][month][cat])
+                    if count < len(dataWithCats[cat]):
+                        count = len(dataWithCats[cat])
+            for cat in dataWithCats.keys():
+                if count > len(dataWithCats[cat]):
+                    for i in range(count - len(dataWithCats[cat])):
+                        dataWithCats[cat].append('')
+
+
+    result.append(firstRow)
+    for cat in dataWithCats:
+        result.append(dataWithCats[cat])
+    return result
+
 def main():
-    parseSheet()
+    data = parseSheet()
+    data = convert_to_list(data)
+    # dataFrame = parseData(get_setting('spreadsheetId'), get_setting('editDiapason'))
+    service = create_write_service()
+    response_date = service.spreadsheets().values().update(
+        spreadsheetId=get_setting('spreadsheetId'),
+        valueInputOption='RAW',
+        range=get_setting('editDiapason'),
+        body=dict(
+            majorDimension='ROWS',
+            values=data
+        )
+    ).execute()
+    print('Sheet successfully Updated')
 
 
 if __name__ == '__main__':
